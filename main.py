40f278a3d92bc6975bf34dc2a4816aed72d94501
piotrekstepien <piotr.stepien.priv@gmail.com>
2022-12-09 23:51:49 +0200

    version09122022

--- main.py
+++ main.py
@@ -1,68 +1,12 @@
 from datetime import date
 import PySimpleGUI as sg
-import requests
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from functions import *
 
 
-def get_date_list(url):  # returns period of time as a list of strings
-    date_dict = get_exchange_data_from_nbp(url)
-    date_list = list(date_dict)
-    return date_list
-
-
-def get_exchange_data_from_nbp(url):
-    response = requests.get(url)
-    dictionary_ = response.json()
-    list_ = dictionary_["rates"]
-    dict_ = {}
-    date_ = []
-    value_ = []
-    for item in list_:
-        date_.append(item["effectiveDate"])
-        value_.append(item["mid"])
-    for item in range(0, len(value_)):
-        dict_[date_[item]] = value_[item]
-    return dict_
-
-
-def get_currency_as_list(
-        url):  # reformats currency exchange rate from a dictionary to a list -> useful for current value of exchange reate
-    currency_dict = get_exchange_data_from_nbp(url)
-    currency_list = list(currency_dict.values())
-    return currency_list
-
-
-def pln_chosen(currency_1, currency_2, date_1, date_2):
-    if currency_1 == "PLN":
-       url1 = "http://api.nbp.pl/api/exchangerates/rates/A/" + f"{currency_2}/{date_1}/{date_2}"
-    if currency_2 == "PLN":
-       url1 = "http://api.nbp.pl/api/exchangerates/rates/A/" + f"{currency_1}/{date_1}/{date_2}"
-    return url1
-
-
-def only_ones(url):
-    currency_list = get_currency_as_list(url)
-    lista = []
-    for i in range(0, len(currency_list)):
-        lista.append(1)
-    return lista
-
-
-def get_currency_ratio(currency_1, currency_2):  # returns ratio of any currencies as a list in specific time period
-    ratio = []
-    for i in range(0, len(currency_1)):
-        ratio.append(round(currency_1[i] / currency_2[i], 4))
-    return ratio
-
-
-today = date.today()
-
-# url = "http://api.nbp.pl/api/exchangerates/rates/A/USD/2022-01-01/" + f"{today}"
-
-
-# currency = get_currency_as_list(url)
-
+today = str(date.today())
+date_to = today
 list_of_currencies = ["USD", "EUR", "BGN", "PLN", "NOK"]
 #
 # Builds GUI:
@@ -76,7 +20,7 @@ layout = [
      sg.CalendarButton(button_text=today, target="-CALENDAR_INPUT_TO-", key="-DATETO-", format=("%Y-%m-%d"),
                        enable_events=True),
      sg.Button("SUBMIT")],
-    [sg.Text("Today rates: "), sg.Text(key="-TODAYRATE-", enable_events=True)],
+    [sg.Text("Today's exchange rate: "), sg.Text(key="-TODAYRATE-", enable_events=True)],
     [sg.Canvas(key="-CANVAS-")]
 ]
 window = sg.Window("X-changeR8 V0.0.1", layout, finalize=True)
@@ -85,16 +29,29 @@ while True:
     event, values = window.read(timeout=5)
     if event == sg.WINDOW_CLOSED:
         break
-    if event == "SUBMIT":
+    if event == "-CALENDAR_INPUT_FROM-":
         date_from = values[
-            "-CALENDAR_INPUT_FROM-"]  # !!!!!!!!!!!SPRAWDZ REFRESH WINDOW --> WINDOW CLOSE -> WINDOW OPEN !!!!!!!!!!!!1
+            "-CALENDAR_INPUT_FROM-"]
         window["-DATEFROM-"].update(text=date_from)
+        window.refresh()
+    if event == "-CALENDAR_INPUT_TO-":
         date_to = values[
-            "-CALENDAR_INPUT_TO-"]  # !!!!!!!!!!!SPRAWDZ REFRESH WINDOW --> WINDOW CLOSE -> WINDOW OPEN !!!!!!!!!!!!1
-        if date_to == "":
-            date_to = today
+            "-CALENDAR_INPUT_TO-"]
+        # if date_to == "":
+        #     date_to = today
         window["-DATETO-"].update(text=date_to)
-
+        window.refresh()
+    if event == "SUBMIT":
+        # date_from = values[
+        #     "-CALENDAR_INPUT_FROM-"]  # !!!!!!!!!!!SPRAWDZ REFRESH WINDOW --> WINDOW CLOSE -> WINDOW OPEN !!!!!!!!!!!!
+        # window["-DATEFROM-"].update(text=date_from)
+        # window.refresh()
+        # date_to = values[
+        #     "-CALENDAR_INPUT_TO-"]  # !!!!!!!!!!!SPRAWDZ REFRESH WINDOW --> WINDOW CLOSE -> WINDOW OPEN !!!!!!!!!!!!1
+        # if date_to == "":
+        #     date_to = today
+        # window["-DATETO-"].update(text=date_to)
+        window.refresh()
         currency1 = values["-CURRENCY1-"]
         currency2 = values["-CURRENCY2-"]
         if currency1 != "PLN" and currency2 != "PLN":
@@ -120,5 +77,6 @@ while True:
         figure_canvas_agg = FigureCanvasTkAgg(fig, window["-CANVAS-"].TKCanvas)
         figure_canvas_agg.draw()
         figure_canvas_agg.get_tk_widget().pack()
+
 
 window.close()
